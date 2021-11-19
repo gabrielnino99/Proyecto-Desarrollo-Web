fb672720098a4fed988851688c44ef90700c0579
DivaLover <poisonjjivy@gmail.com>
2021-11-18 22:59:59 -0500

    modified:   proyectodw/src/main/java/com/example/proyectodw/model/SpringBootConsoleApplication.java

--- proyectodw/src/main/java/com/example/proyectodw/model/SpringBootConsoleApplication.java
+++ proyectodw/src/main/java/com/example/proyectodw/model/SpringBootConsoleApplication.java
@@ -18,25 +18,25 @@ import org.springframework.context.annotation.Profile;
 @Profile("default")
 public class SpringBootConsoleApplication implements CommandLineRunner{
     private static final Logger LOG = LoggerFactory.getLogger(SpringBootConsoleApplication.class);
-    /*
+    
     public static final int NUM_ESTRELLAS = 40000;
     public static final int NUM_PRODUCTOS = 500;
     public static final int NUM_NAVES = 20;
     public static final int NUM_USUARIOS = 100;
     public static final int NUM_EQUIPOS = 10;
-    */
+    
 
     //DATOS DE PRUEBA
     //Si mil estrellas tardan 3 minutos en subir, estimando matematicamente
     //40 mil estrellas tardarian 120 minutos, es decir, 2 horas
     
-    
+    /*
     public static final int NUM_ESTRELLAS = 1000;
     public static final int NUM_PRODUCTOS = 500;
     public static final int NUM_NAVES = 20;
     public static final int NUM_USUARIOS = 100;
     public static final int NUM_EQUIPOS = 10;
-    
+    */
     
 
     @Autowired
